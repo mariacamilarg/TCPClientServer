bd25bce76ccc9c63db613e2337b202c56ff65e07
Maria Camila Remolina Gutierrez <mariacamilaremolinagutierrez@gmail.com>
2017-10-05 18:42:53 -0500

    protocol working

--- client/client.py
+++ client/client.py
@@ -1,16 +1,40 @@
 #!/usr/bin/env python
 
 import socket
+import protocol_client as pc
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-MESSAGE = "Hello, World!"
+
+def send_to_server(sckt, msg):
+    sckt.sendto(msg.encode(), (TCP_IP,TCP_PORT))
+    print("> CLIENT:", msg)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
 
-print "received data:", data
+# Ask for files list
+send_to_server(s, pc.START)
+data = s.recv(BUFFER_SIZE)
+files = data.decode()
+print("< SERVER:", files)
+
+# Ask for specific file
+send_to_server(s, pc.SEND_FILE)
+filename = "1.txt"
+send_to_server(s, filename)
+
+# Receive file
+data = s.recv(BUFFER_SIZE)
+file_start = data.decode()
+print("< SERVER:", file_start)
+file_size = int(file_start.split(":")[1])
+send_to_server(s, pc.OK)
+data = s.recv(file_size)
+desired_file = data.decode()
+print("< SERVER:", desired_file)
+
+# End connection
+send_to_server(s, pc.END)
+s.close()
--- server/server.py
+++ server/server.py
@@ -1,22 +1,46 @@
 #!/usr/bin/env python
 
 import socket
+import protocol_server as ps
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
-BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+BUFFER_SIZE = 64  # Normally 1024, but we want fast response
+
+def send_to_client(cnn, msg):
+    conn.send(msg.encode())
+    print("> SERVER:", msg)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
+print("Waiting for connections...")
 
+# conn is a new socket object usable to send and receive data on the connection
+# addr is the address bound to the socket on the other end of the connection
 conn, addr = s.accept()
-print('Connection address:', addr)
+print('Connection created:', addr)
 
 while True:
     data = conn.recv(BUFFER_SIZE)
-    if not data: break
-    print "received data:", data
-    conn.send(data)  # echo
+    data_d = data.decode()
+    print("< CLIENT:", data_d)
+
+    if data_d == ps.START:
+        files = ['1.txt', '2.txt', '3.txt', '4.txt', '5.txt']
+        send_to_client(conn, str(files))
+    elif data_d == ps.SEND_FILE:
+        filename = conn.recv(BUFFER_SIZE)
+        filename_d = filename.decode()
+        print("< CLIENT:", filename_d)
+        desired_file = "File..."
+        desired_file_size = len(desired_file.encode('utf-8'))
+        send_to_client(conn, ps.START_OF_FILE + ":" + str(desired_file_size))
+        print("< CLIENT:", conn.recv(BUFFER_SIZE).decode())
+        send_to_client(conn, desired_file)
+    elif data_d == ps.END:
+        break
+    elif not data:
+        break
 
 conn.close()
